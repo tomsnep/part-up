8cae15853815cc407ff9b3c266d132da1919c23a
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2016-01-18 17:00:17 +0100

    WIP Mobile notifications with header
    updated ignore

--- app/packages/partup:client-pages/mobile/notifications/notifications.js
+++ app/packages/partup:client-pages/mobile/notifications/notifications.js
@@ -1,8 +1,11 @@
 Template.mobile_notifications.onCreated(function() {
     var self = this;
-    self.subscribe('notifications.for_upper', {
-        onReady: function() {
-            console.log('aaa?');
-        }
-    })
+    self.subscribe('notifications.for_upper', 25)
+})
+
+Template.mobile_notifications.helpers({
+    notifications: function() {
+        var parameters = {sort: {created_at: -1}, limit: 25};
+        return Notifications.findForUser(Meteor.user(), {}, parameters);
+    }
 })
