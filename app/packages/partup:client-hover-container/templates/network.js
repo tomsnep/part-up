c88c53d1572286bc131e3348d02a96dfa38701f5
Léon Smit <lee_smit@live.nl>
2016-02-15 10:05:38 +0100

    fix(swarm): network tile stats

--- app/packages/partup:client-hover-container/templates/network.js
+++ app/packages/partup:client-hover-container/templates/network.js
@@ -1,13 +1,11 @@
 Template.HoverContainer_network.onCreated(function() {
     var networkSlug = this.data;
-    this.subscribe('networks.one', networkSlug);
 });
 
 Template.HoverContainer_network.helpers({
-    network: function() {
+    networkSlug: function() {
         var networkSlug = Template.instance().data;
-        var network = Networks.guardedMetaFind({slug: networkSlug}, {limit: 1}).fetch().pop() || null;
-        if (!network) return;
-        return network;
+        if (!networkSlug) return;
+        return networkSlug;
     }
 });
--- app/packages/partup:client-network-tile/NetworkTile.js
+++ app/packages/partup:client-network-tile/NetworkTile.js
@@ -1,17 +1,27 @@
 Template.NetworkTile.onCreated(function() {
-    var network = this.data.network;
+    var networkSlug = this.data.networkSlug;
+    this.subscribe('networks.one', networkSlug);
 });
 
 Template.NetworkTile.helpers({
-    networkLogo: function() {
-        var network = this.network;
+    data: function() {
+        var networkSlug = this.networkSlug;
+        var network = Networks.findOne({slug: networkSlug});
+        return {
+            networkLogo: function() {
+                var network = this.network;
 
-        if (network.logoObject) {
-            return Partup.helpers.url.getImageUrl(network.logoObject, '360x360');
-        } else if (network.imageObject) {
-            return Partup.helpers.url.getImageUrl(network.imageObject, '360x360');
-        }
+                if (network.logoObject) {
+                    return Partup.helpers.url.getImageUrl(network.logoObject, '360x360');
+                } else if (network.imageObject) {
+                    return Partup.helpers.url.getImageUrl(network.imageObject, '360x360');
+                }
 
-        return '';
+                return '';
+            },
+            network: function() {
+                return network;
+            }
+        };
     }
-})
+});
