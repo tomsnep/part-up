baa1b5dc3c6c8ac9dac1d16cdd353c9463a8b8b7
Nick Koster <nick.koster@lifely.nl>
2016-02-15 18:03:48 +0100

    feat(swarms): refactored related networks

--- app/packages/partup:server/methods/swarms/swarms_methods.js
+++ app/packages/partup:server/methods/swarms/swarms_methods.js
@@ -384,21 +384,14 @@ Meteor.methods({
             var upper = Meteor.user();
             if (!upper) return swarm_networks;
             var upper_tags = upper.tags || [];
-            var related_networks = [];
 
-            // Loop through networks to check common tags
-            swarm_networks.forEach(function(networkId) {
-                var network = Networks.findOne(networkId);
-                var network_tags = network.tags || [];
-                var common_tags_count = lodash.intersection(upper_tags, network_tags).length;
-                related_networks.push({'network_id': network._id, 'common_tags_count': common_tags_count});
-            });
-
-            // Order the array based on tags count
-            related_networks = lodash.sortBy(related_networks, 'common_tags_count');
-
-            // And there we have it
-            return related_networks;
+            // Loop through networks to sort by common tags
+            return swarm_networks
+                .sort(function(networkId) {
+                    var network = Networks.findOne(networkId);
+                    var network_tags = network.tags || [];
+                    return lodash.intersection(upper_tags, network_tags).length;
+                });
         } catch (error) {
             Log.error(error);
             throw new Meteor.Error(400, 'related_networks_could_not_be_given');
