2b2b99bc116bb0abf644dee9bab5b7b87db9805b
Nick Koster <nick.koster@lifely.nl>
2016-02-15 14:33:07 +0100

    feat(swarms): finalized stats service

--- app/packages/partup:server/cron/update_swarm_stats.js
+++ app/packages/partup:server/cron/update_swarm_stats.js
@@ -1,6 +1,6 @@
-if(process.env.PARTUP_CRON_ENABLED) {
+if (process.env.PARTUP_CRON_ENABLED) {
     SyncedCron.add({
-        name: 'Update the stats of all swarms',
+        name: 'Update the swarm and network stats of Part-up',
         schedule: function(parser) {
             return parser.text(Partup.constants.CRON_SWARM_UPDATE_STATS);
         },
--- app/packages/partup:server/services/swarms_service.js
+++ app/packages/partup:server/services/swarms_service.js
@@ -13,35 +13,50 @@ Partup.server.services.swarms = {
      */
     updateStats: function(swarm) {
         // Initialize stats counters
-        var stats = {
+        var swarm_stats = {
             activity_count: 0,
             network_count: 0,
+            partner_count: 0,
             partup_count: 0,
             supporter_count: 0,
             upper_count: 0
         };
 
-        Log.debug('SwID', swarm._id);
         // Get all the network IDs to loop through
         var networks = swarm.networks || [];
 
         // Set the network count
-        stats.network_count = networks.length;
+        swarm_stats.network_count = networks.length;
 
         // Loop through each of the networks to get all partups
         networks.forEach(function(networkId) {
             // Retrieve the network
             var network = Networks.findOne(networkId);
 
+            // Initialize stats counters
+            var network_stats = {
+                activity_count: 0,
+                partner_count: 0,
+                partup_count: 0,
+                supporter_count: 0,
+                upper_count: 0
+            };
+
             // Store the partup IDs to loop through for the activities
-            var network_partups = network.partups || [];
+            var partups = Partups.find({network_id: network._id}, {_id: 1}).fetch();
+            var network_partups = [];
+            partups.forEach(function(partup) {
+                network_partups.push(partup._id);
+            });
 
             // Update the partup counter with the amount of partups in this network
-            stats.partup_count += network_partups.length;
+            network_stats.partup_count = network_partups.length;
+            swarm_stats.partup_count += network_stats.partup_count;
 
             // Update the upper counter with the amount of uppers in this network
             var network_uppers = network.uppers || [];
-            stats.upper_count += network_uppers.length;
+            network_stats.upper_count = network_uppers.length;
+            swarm_stats.upper_count += network_uppers.length;
 
             // Loop through each partup to get activity and supporter count
             network_partups.forEach(function(partupId) {
@@ -49,21 +64,34 @@ Partup.server.services.swarms = {
                 var partup = Partups.findOne(partupId);
 
                 // Update the activity counter with the amount of activities in this partup
-                stats.activity_count += partup.activity_count;
+                var partup_activity_count = partup.activity_count || 0;
+                network_stats.activity_count += partup_activity_count;
+                swarm_stats.activity_count += partup_activity_count;
+
+                // Update the partner counter
+                var partup_partners = partup.uppers || [];
+                network_stats.partner_count += partup_partners.length;
+                swarm_stats.partner_count += partup_partners.length;
 
                 // And lastly, update the supporter counter
                 var partup_supporters = partup.supporters || [];
-                stats.supporter_count += partup_supporters.length;
+                network_stats.supporter_count += partup_supporters.length;
+                swarm_stats.supporter_count += partup_supporters.length;
+            });
+
+            // Update the network with the new stats
+            Networks.update(network._id, {
+                $set: {
+                    stats: network_stats,
+                    updated_at: new Date()
+                }
             });
         });
-
-        // Some logging
-        Log.debug('Swarm stats calculated:', stats);
 
         // Update the swarm with the new stats
         Swarms.update(swarm._id, {
             $set: {
-                stats: stats,
+                stats: swarm_stats,
                 updated_at: new Date()
             }
         });
