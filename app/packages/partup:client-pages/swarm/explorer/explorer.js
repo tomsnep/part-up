99fcacd5f110c54825710c3db3cbdccb8fbca4ea
Léon Smit <lee_smit@live.nl>
2016-02-18 22:52:12 +0100

    fix(swarm): changed limit and min for inner circle

--- app/packages/partup:client-pages/swarm/explorer/explorer.js
+++ app/packages/partup:client-pages/swarm/explorer/explorer.js
@@ -1,6 +1,6 @@
 Template.swarm_explorer.onCreated(function() {
     this.rendered = new ReactiveVar(false);
-    this.pageLimit = 13;
+    this.pageLimit = 12;
 });
 Template.swarm_explorer.onRendered(function() {
     this.rendered.set(true);
@@ -30,17 +30,17 @@ Template.swarm_explorer.helpers({
                 var empty = total ? false : true;
                 if (total > template.pageLimit) {
                     var total = Math.ceil(total / 2);
-                    var numberOfInnerRingItems = Math.min(4, Math.ceil(total / 3));
+                    var numberOfInnerRingItems = Math.min(4, Math.max(Math.ceil(total / 3), 2));
 
                     var pages = [{number: 1, inner: [], outer: []},
                                  {number: 2, inner: [], outer: []}];
 
                     _.each(pages, function(item, index) {
                         _.times(numberOfInnerRingItems, function() {
-                            item.inner.push(networks.pop());
+                            if (networks.length) item.inner.push(networks.pop());
                         });
                         _.times((total - item.inner.length), function() {
-                            if(networks.length) item.outer.push(networks.pop());
+                            if (networks.length) item.outer.push(networks.pop());
                         });
                     });
                     return pages;
@@ -49,11 +49,11 @@ Template.swarm_explorer.helpers({
 
                     // number of inner ring items
                     // 1/3 of total, max 4
-                    var numberOfInnerRingItems = Math.min(4, Math.ceil(total / 3));
+                    var numberOfInnerRingItems = Math.min(4, Math.max(Math.ceil(total / 3), 2));
 
                     if (!empty) {
                         _.times(numberOfInnerRingItems, function() {
-                            innerRingItems.push(networks.pop());
+                            if (networks.length) innerRingItems.push(networks.pop());
                         });
                         outerRingItems = networks;
                     }
