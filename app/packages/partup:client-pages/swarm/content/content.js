f9ab18e83ad06432fe523a6708a9922e889d1967
Léon Smit <lee_smit@live.nl>
2016-02-18 11:02:54 +0100

    feat(swarm): related tribes list is scrollable

---
+++ app/packages/partup:client-pages/swarm/content/content.js
@@ -0,0 +1,32 @@
+Template.swarm_content.onRendered(function() {
+    var template = this;
+    var mobile = window.outerWidth < 480;
+
+    template.scrollElement = $(template.find('[data-horizontal-scroll]'));
+    // remember scrolloffset;
+    var oldScrollOffsetLeft = 0;
+    // mousewheel handler
+    template.mouseWheelHandler = function(e) {
+        var scrollLeft = template.scrollElement[0].scrollLeft;
+        var scrollEnd = ((template.scrollElement[0].offsetWidth + scrollLeft) > template.scrollElement[0].scrollWidth - 800);
+        var scrollDirection = oldScrollOffsetLeft < scrollLeft ? 'right' : 'left';
+        oldScrollOffsetLeft = scrollLeft + 10;
+        if (e.type === 'mousewheel') {
+            this.scrollLeft -= e.originalEvent.wheelDeltaY;
+            if (!e.originalEvent.wheelDeltaX) {
+                e.preventDefault();
+            }
+        } else {
+            e.preventDefault();
+            this.scrollLeft += (e.originalEvent.detail * 5);
+        }
+    };
+
+    if (!mobile) {
+        template.scrollElement.on('mousewheel DOMMouseScroll', template.mouseWheelHandler);
+    }
+});
+
+Template.swarm_content.onDestroyed(function() {
+    template.scrollElement.off('mousewheel DOMMouseScroll', template.mouseWheelHandler);
+});
