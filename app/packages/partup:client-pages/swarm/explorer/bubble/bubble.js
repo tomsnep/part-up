0ba1009a67bee9f67435ec273d2172d6ede5578f
Léon Smit <lee_smit@live.nl>
2016-02-19 10:55:38 +0100

    fix(swarm): ie fallback for swarm header

--- app/packages/partup:client-pages/swarm/explorer/bubble/bubble.js
+++ app/packages/partup:client-pages/swarm/explorer/bubble/bubble.js
@@ -55,6 +55,21 @@ Template.Bubble.helpers({
             },
             active: function() {
                 return template.active.get();
+            },
+            isIE: function() {
+                var ua = window.navigator.userAgent;
+                var msie = ua.indexOf('MSIE ');
+
+                if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+                    return true;
+                }
+
+                if (/Edge/.test(navigator.userAgent)) {
+                    // this is Microsoft Edge
+                    return true;
+                }
+
+                return false;
             }
         };
     }
