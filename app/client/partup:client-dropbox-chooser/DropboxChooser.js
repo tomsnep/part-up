d1559f8043b3a637ccf8790a215205dc77b5ada6
Theuy Limpanont <theuy.limpanont@gmail.com>
2016-02-26 14:39:53 +0100

    feat(dropbox-chooser): include check totalPhotos count

--- app/client/partup:client-dropbox-chooser/DropboxChooser.js
+++ app/client/partup:client-dropbox-chooser/DropboxChooser.js
@@ -10,24 +10,29 @@ if (Meteor.isClient) {
         var button = Dropbox.createChooseButton({
             // Required. Called when a user selects an item in the Chooser.
             success: function(files) {
-                var directLinkUrl = files[0].link;
-                template.uploadingPhotos.set(true);
 
-                Partup.client.uploader.uploadImageByUrl(directLinkUrl, function(error, image) {
-                    template.uploadingPhotos.set(false);
-                    if (error) {
-                        Partup.client.notify.error(TAPi18n.__(error.reason));
-                        return;
-                    }
-                    var uploaded = template.uploadedPhotos.get();
-                    uploaded.push(image._id);
-                    template.uploadedPhotos.set(uploaded);
+                template.uploadingPhotos.set(true);
+                var total = template.totalPhotos.get();
+                files.forEach(function(file) {
+                    if (total === template.maxPhotos) return;
+
+                    Partup.client.uploader.uploadImageByUrl(file.link, function(error, image) {
+                        template.uploadingPhotos.set(false);
+                        if (error) {
+                            Partup.client.notify.error(TAPi18n.__(error.reason));
+                            return;
+                        }
+                        var uploaded = template.uploadedPhotos.get();
+                        uploaded.push(image._id);
+                        template.uploadedPhotos.set(uploaded);
+                    });
+                    total++;
+                    template.totalPhotos.set(total);
                 });
-                template.totalPhotos.set(1);
             },
             cancel: function() {},
             linkType: "direct", // or "direct"
-            multiselect: false, // or true
+            multiselect: true, // or true
             extensions: ['.jpg', '.png']
         });
 
