9167d6cae7999cf6d7cf149896c5414d247588c1
Léon Smit <lee_smit@live.nl>
2016-02-29 16:49:50 +0100

    refactor(network): moved the join tribe button to it's own template

--- app/packages/partup:client-pages/app/network/network.js
+++ app/packages/partup:client-pages/app/network/network.js
@@ -6,8 +6,6 @@ var Subs = new SubsManager({
 Template.app_network.onCreated(function() {
     var tpl = this;
 
-    tpl.joinToggle = new ReactiveVar(false);
-
     tpl.networkId = new ReactiveVar();
 
     var network_sub;
@@ -79,144 +77,15 @@ Template.app_network.helpers({
         return false;
     },
 
-    joinToggle: function() {
-        return Template.instance().joinToggle.get();
-    },
-
     accessToken: function() {
         return Template.instance().data.accessToken;
     }
 });
 
-// The 'networks.joins' method handles the different possible states (uninvited or invited)
-var joinNetworkOrAcceptInvitation = function(slug) {
-    var network = Networks.findOne({slug: slug});
-    Meteor.call('networks.join', network._id, function(error) {
-        if (error) {
-            Partup.client.notify.error(error.reason);
-        } else {
-            Partup.client.notify.success('Joined network');
-
-            analytics.track('joined network', {
-                networkId: network._id
-            });
-        }
-    });
-};
-
-var leaveNetwork = function(template, network) {
-    Meteor.call('networks.leave', network._id, function(error) {
-        if (error) {
-            Partup.client.notify.error(error.reason);
-            return;
-        }
-        template.joinToggle.set(!template.joinToggle.get());
-
-        Partup.client.notify.success(TAPi18n.__('pages-app-network-notification-left'));
-        Subs.reset();
-        if (network.isClosedForUpper(Meteor.user())) {
-            Router.go('discover');
-        }
-        analytics.track('left network', {
-            networkId: network._id
-        });
-    });
-};
-
 /*************************************************************/
 /* Page events */
 /*************************************************************/
 Template.app_network.events({
-    'click [data-join]': function(event, template) {
-        event.preventDefault();
-        var user = Meteor.user();
-
-        var proceed = function() {
-            var network = Networks.findOne({slug: template.data.networkSlug});
-            Meteor.call('networks.join', network._id, function(error) {
-                if (error) return Partup.client.notify.error(error.reason);
-                template.joinToggle.set(!template.joinToggle.get());
-
-                if (network.isClosed()) {
-                    Partup.client.notify.success(TAPi18n.__('pages-app-network-notification-accepted_waitingforapproval'));
-                } else {
-                    Partup.client.notify.success(TAPi18n.__('pages-app-network-notification-joined'));
-                    analytics.track('joined network', {
-                        networkId: network._id
-                    });
-                }
-            });
-        };
-
-        if (user) {
-            proceed();
-        } else {
-            Intent.go({route: 'login'}, function(loggedInUser) {
-                if (loggedInUser) proceed();
-                else Partup.client.notify.error(TAPi18n.__('pages-app-network-notification-failed'));
-            });
-        }
-    },
-    'click [data-accept]': function(event, template) {
-        event.preventDefault();
-        var proceedAccept = function(user) {
-            var network = Networks.findOne({slug: template.data.networkSlug});
-            Meteor.call('networks.join', network._id, function(error) {
-                if (error) return Partup.client.notify.error(error.reason);
-                template.joinToggle.set(!template.joinToggle.get());
-                if (!network.isClosed()) {
-                    Partup.client.notify.success(TAPi18n.__('pages-app-network-notification-joined'));
-                }
-            });
-        }
-        var user = Meteor.user();
-        if (!user) {
-            Intent.go({route: 'login'}, function(loggedInUser) {
-                if (loggedInUser) proceedAccept(loggedInUser);
-                else Partup.client.notify.error(TAPi18n.__('pages-app-network-notification-failed'));
-            });
-            return
-        }
-        proceedAccept(user);
-    },
-    'click [data-leave]': function(event, template) {
-        event.preventDefault();
-        var network = Networks.findOne({slug: template.data.networkSlug});
-
-        Partup.client.prompt.confirm({
-            title: TAPi18n.__('pages-app-network-confirmation-title', {
-                tribe: network.name
-            }),
-            message: TAPi18n.__('pages-app-network-confirmation-message'),
-            confirmButton: TAPi18n.__('pages-app-network-confirmation-confirm-button'),
-            cancelButton: TAPi18n.__('pages-app-network-confirmation-cancel-button'),
-            onConfirm: function() {
-                leaveNetwork(template, network);
-            }
-        });
-
-    },
-    'click [data-request-invite]': function(event, template) {
-        event.preventDefault();
-
-        var requestInvite = function() {
-            var network = Networks.findOne({slug: template.data.networkSlug});
-            Meteor.call('networks.join', network._id, function(err) {
-                if (err) {
-                    console.error(err);
-                    Partup.client.notify.error(TAPi18n.__(err));
-                }
-            });
-        };
-
-        if (Meteor.user()) {
-            requestInvite();
-        } else {
-            Intent.go({route: 'login'}, function() {
-                requestInvite();
-            });
-        }
-    },
     'click [data-expand]': function(event, template) {
         event.preventDefault();
         template.expandText(!template.expanded);
