2b3fcd6f128ccfdefb8b1973faa7d040fc629d52
Léon Smit <lee_smit@live.nl>
2016-03-01 16:37:40 +0100

    fix(tribe): routing implemented in user menu and partup tiles, also refactored the network page a bit

--- app/packages/partup:client-pages/app/network/network.js
+++ app/packages/partup:client-pages/app/network/network.js
@@ -1,84 +1,64 @@
-var Subs = new SubsManager({
-    cacheLimit: 1,
-    expireIn: 10
-});
-
 Template.app_network.onCreated(function() {
-    var tpl = this;
+    var template = this;
+    var networkSlug = template.data.networkSlug;
+    template.networkSlug = new ReactiveVar(networkSlug);
+    template.networkLoaded = new ReactiveVar(false);
+    template.autorun(function() {
+        var data = Template.currentData();
+        var slug = data.networkSlug;
 
-    tpl.networkId = new ReactiveVar();
-
-    var network_sub;
-
-    tpl.autorun(function() {
-        var slug = Template.currentData().networkSlug;
-
-        network_sub = Subs.subscribe('networks.one', slug, {
+        template.networkSlug.set(slug);
+        if (template.networkSubscription) template.networkSubscription.stop();
+        template.networkLoaded.set(false);
+        template.networkSubscription = template.subscribe('networks.one', slug, {
             onReady: function() {
-                var network = Networks.findOne({slug: tpl.data.networkSlug});
+                var network = Networks.findOne({slug: slug});
                 if (!network) Router.pageNotFound('network');
-                tpl.networkId.set(network._id);
+                template.networkLoaded.set(true);
             }
         });
-    });
-    tpl.expanded = false;
-    tpl.autorun(function() {
-        var scrolled = Partup.client.scroll.pos.get() > 100;
-        if (scrolled && tpl.expanded) {
-            if (tpl.view.isRendered) {
-                tpl.expandText(false);
-            }
-        }
-    });
-    tpl.expandText = function(expand) {
-        var clickedElement = $('[data-expand]');
-        if (!clickedElement || !clickedElement[0]) return;
-        var parentElement = $(clickedElement[0].parentElement);
 
-        var collapsedText = TAPi18n.__(clickedElement.data('collapsed-key')) || false;
-        var expandedText = TAPi18n.__(clickedElement.data('expanded-key')) || false;
-
-        if (expand) {
-            if (expandedText) clickedElement.html(expandedText);
-            parentElement.addClass('pu-state-open');
-            clickedElement.parents('.pu-sub-pageheader').addClass('pu-state-descriptionexpanded');
-        } else {
-            if (collapsedText) clickedElement.html(collapsedText);
-            parentElement.removeClass('pu-state-open');
-            clickedElement.parents('.pu-sub-pageheader').removeClass('pu-state-descriptionexpanded');
-        }
-        tpl.expanded = expand;
-    };
+    });
 });
 
 /*************************************************************/
 /* Page helpers */
 /*************************************************************/
 Template.app_network.helpers({
-    network: function() {
-        return Networks.findOne({slug: this.networkSlug});
-    },
-
-    isInvitePending: function() {
-        var user = Meteor.user();
-        if (!user || !user.pending_networks) return false;
-        return mout.array.contains(user.pending_networks, this.networkId);
-    },
-
-    shrinkHeader: function() {
-        return Partup.client.scroll.pos.get() > 100;
-    },
-    textHasOverflown: function() {
+    data: function() {
         var template = Template.instance();
-        var rendered = template.partupTemplateIsRendered.get();
-        if (!rendered) return;
-        var expander = $(template.find('[data-expander-parent]'));
-        if (expander.length && expander[0].scrollHeight > expander.innerHeight()) return true;
-        return false;
+
+        var networkSlug = template.networkSlug.get();
+        if (!networkSlug) return false;
+
+        var network = Networks.findOne({slug: networkSlug});
+        if (!network) return false;
+
+        return {
+            network: function() {
+                return network;
+            },
+            isInvitePending: function() {
+                var user = Meteor.user();
+                if (!user || !user.pending_networks) return false;
+                return mout.array.contains(user.pending_networks, network._id);
+            },
+            accessToken: function() {
+                return data.accessToken;
+            },
+            networkSlug: function() {
+                return template.networkSlug.get();
+            }
+        };
     },
 
-    accessToken: function() {
-        return Template.instance().data.accessToken;
+    state: function() {
+        var template = Template.instance();
+        return {
+            shrinkHeader: function() {
+                return Partup.client.scroll.pos.get() > 100;
+            }
+        };
     }
 });
 
@@ -86,22 +66,20 @@ Template.app_network.helpers({
 /* Page events */
 /*************************************************************/
 Template.app_network.events({
-    'click [data-expand]': function(event, template) {
-        event.preventDefault();
-        template.expandText(!template.expanded);
-    },
     'click [data-open-networksettings]': function(event, template) {
         event.preventDefault();
+        var networkSlug = template.networkSlug.get();
         Intent.go({
             route: 'network-settings',
             params: {
-                slug: template.data.networkSlug
+                slug: networkSlug
             }
         });
     },
     'click [data-location]': function(event, template) {
         event.preventDefault();
-        var location = Networks.findOne({slug: template.data.networkSlug}).location;
+        var networkSlug = template.networkSlug.get();
+        var location = Networks.findOne({slug: networkSlug}).location;
         Partup.client.discover.setPrefill('locationId', location.place_id);
         Partup.client.discover.setCustomPrefill('locationLabel', location.city);
         Router.go('discover');
