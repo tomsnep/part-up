3b186e89c230884158a80d1fd8aa96e093ad314a
Léon Smit <lee_smit@live.nl>
2016-03-02 16:53:37 +0100

    feat(tribestart): fully working start page with party accordion

--- app/packages/partup:client-pages/app/network/partials/partups.js
+++ app/packages/partup:client-pages/app/network/partials/partups.js
@@ -1,10 +1,51 @@
+Template.app_network_start_partups.onCreated(function() {
+    var template = this;
+
+    template.activeImage = new ReactiveVar(template.data.partups.fetch().shift().image);
+    template.maxPartups = 3;
+
+});
 Template.app_network_start_partups.helpers({
     data: function() {
         var data = Template.currentData();
+        var template = Template.instance();
+        var partups = data.partups.fetch();
+        var partupCount = data.partups.count();
+
         return {
             partups: function() {
-                return data.partups || [];
+                var listPartups = [];
+                _.times(template.maxPartups, function() {
+                    if (!partups.length) return;
+                    var partup = partups.shift();
+                    partup.activityCount = partup.activity_count || Activities.findForPartup(partup).count();
+                    partup.supporterCount = partup.supporters ? partup.supporters.length : 0;
+                    partup.dayCount = Math.ceil(((((new Date() - new Date(partup.created_at)) / 1000) / 60) / 60) / 24);
+                    listPartups.push(partup);
+                });
+                return listPartups;
+            },
+            remainingPartups: function() {
+                var remaining = partupCount > template.maxPartups ? partupCount - template.maxPartups : 0;
+                return remaining;
+            }
+        };
+    },
+    state: function() {
+        var template = Template.instance();
+        return {
+            activeImage: function() {
+                return template.activeImage.get();
             }
         };
     }
+});
+
+Template.app_network_start_partups.events({
+    'mouseenter [data-expand]': function(event, template) {
+        template.activeImage.set(this.image);
+    },
+    // 'mouseleave [data-stack]': function(event, template) {
+    //     template.activeImage.set(template.data.partups.fetch().shift().image);
+    // }
 });
--- app/packages/partup:client-pages/app/network/partials/uppers.js
+++ app/packages/partup:client-pages/app/network/partials/uppers.js
@@ -1,14 +1,20 @@
 Template.app_network_start_uppers.onCreated(function() {
     var template = this;
     template.subscribe('users.by_ids', template.data.uppers);
+    template.maxUppers = 7;
 });
 Template.app_network_start_uppers.helpers({
     data: function() {
         var template = Template.instance();
         var upperIds = template.data.uppers || [];
+        var upperCount = upperIds.length;
         return {
             uppers: function() {
-                return Meteor.users.find({_id: {$in: upperIds}});
+                return Meteor.users.find({_id: {$in: upperIds}}, {limit: template.maxUppers});
+            },
+            remainingUppers: function() {
+                var remaining = upperCount > template.maxUppers ? upperCount - template.maxUppers : 0;
+                return remaining;
             }
         };
     }
