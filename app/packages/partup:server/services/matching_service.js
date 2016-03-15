787b4eb68bc9c28c5e86d2d23f866f8f4fc15cb5
Nick Koster <nick.koster@lifely.nl>
2016-03-15 15:03:04 +0100

    refactor(matching): removed duplicate code and created a generic matching function that can be used by all methods

--- app/packages/partup:server/services/matching_service.js
+++ app/packages/partup:server/services/matching_service.js
@@ -17,58 +17,10 @@ Partup.server.services.matching = {
     matchUppersForActivity: function(activityId, searchOptions) {
         var activity = Activities.findOneOrFail(activityId);
         var partup = Partups.findOneOrFail(activity.partup_id);
+        var tags = partup.tags || [];
+        var uppers = partup.uppers || [];
 
-        var selector = {};
-        var options = {};
-        var searchOptionsProvided = searchOptions.locationId || searchOptions.query;
-        var limit = searchOptionsProvided ? 30 : 10;
-
-        if (searchOptionsProvided) {
-            if (searchOptions.locationId) {
-                selector['profile.location.place_id'] = searchOptions.locationId;
-            }
-            if (searchOptions.query) {
-                // Remove accents that might have been added to the query
-                searchOptions.query = mout.string.replaceAccents(searchOptions.query.toLowerCase());
-                selector['profile.normalized_name'] = new RegExp('.*' + searchOptions.query + '.*', 'i');
-            }
-        } else {
-            // Match the uppers on the tags used in the partup
-            var tags = partup.tags || [];
-            selector['profile.tags'] = {'$in': tags};
-        }
-
-        // Exclude the current logged in user from the results
-        selector['_id'] = {'$nin': [Meteor.userId()]};
-
-        // Exclude existing partners from result, unless a search is happening
-        if (!searchOptionsProvided) {
-            selector['_id'] = {'$nin': partup.uppers};
-        }
-
-        // Sort the uppers on participation_score
-        options['sort'] = {'participation_score': -1};
-
-        // Only return the IDs
-        options['fields'] = {'_id': 1};
-
-        // Limit results
-        options['limit'] = limit;
-
-        var results = Meteor.users.findActiveUsers(selector, options).fetch();
-
-        // If there are no results, we remove some matching steps (only when no search options were provided)
-        if (!searchOptionsProvided) {
-            var iteration = 0;
-            while (results.length === 0) {
-                if (iteration === 0) delete selector['profile.tags'];
-
-                results = Meteor.users.findActiveUsers(selector, options).fetch();
-                iteration++;
-            }
-        }
-
-        return results;
+        return this.findMatchingUppers(searchOptions, tags, uppers);
     },
 
     /**
@@ -83,58 +35,10 @@ Partup.server.services.matching = {
      */
     matchUppersForNetwork: function(networkId, searchOptions) {
         var network = Networks.findOneOrFail(networkId);
+        var tags = network.tags || [];
+        var uppers = network.uppers || [];
 
-        var selector = {};
-        var options = {};
-        var searchOptionsProvided = searchOptions.locationId || searchOptions.query;
-        var limit = searchOptionsProvided ? 30 : 10;
-
-        if (searchOptionsProvided) {
-            if (searchOptions.locationId) {
-                selector['profile.location.place_id'] = searchOptions.locationId;
-            }
-            if (searchOptions.query) {
-                // Remove accents that might have been added to the query
-                searchOptions.query = mout.string.replaceAccents(searchOptions.query.toLowerCase());
-                selector['profile.normalized_name'] = new RegExp('.*' + searchOptions.query + '.*', 'i');
-            }
-        } else {
-            // Match the uppers on the tags used in the network
-            var tags = network.tags || [];
-            selector['profile.tags'] = {'$in': tags};
-        }
-
-        // Exclude the current logged in user from the results
-        selector['_id'] = {'$nin': [Meteor.userId()]};
-
-        // Exclude existing partners from result, unless a search is happening
-        if (!searchOptionsProvided) {
-            selector['_id'] = {'$nin': network.uppers};
-        }
-
-        // Sort the uppers on participation_score
-        options['sort'] = {'participation_score': -1};
-
-        // Only return the IDs
-        options['fields'] = {'_id': 1};
-
-        // Limit results
-        options['limit'] = limit;
-
-        var results = Meteor.users.findActiveUsers(selector, options).fetch();
-
-        // If there are no results, we remove some matching steps (only when no search options were provided)
-        if (!searchOptionsProvided) {
-            var iteration = 0;
-            while (results.length === 0) {
-                if (iteration === 0) delete selector['profile.tags'];
-
-                results = Meteor.users.findActiveUsers(selector, options).fetch();
-                iteration++;
-            }
-        }
-
-        return results;
+        return this.findMatchingUppers(searchOptions, tags, uppers);
     },
 
     /**
@@ -149,7 +53,22 @@ Partup.server.services.matching = {
      */
     matchUppersForPartup: function(partupId, searchOptions) {
         var partup = Partups.findOneOrFail(partupId);
+        var tags = partup.tags || [];
+        var uppers = partup.uppers || [];
 
+        return this.findMatchingUppers(searchOptions, tags, uppers);
+    },
+
+    /**
+     * Find upeprs that match with the provided criteria
+     *
+     * @param {Object} searchOptions
+     * @param {[String]} tags
+     * @param {[String]} excludedUppers
+
+     * @return {[String]}
+     */
+    findMatchingUppers: function(searchOptions, tags, excludedUppers) {
         var selector = {};
         var options = {};
         var searchOptionsProvided = searchOptions.locationId || searchOptions.query;
@@ -165,24 +84,25 @@ Partup.server.services.matching = {
                 selector['profile.normalized_name'] = new RegExp('.*' + searchOptions.query + '.*', 'i');
             }
         } else {
-            // Match the uppers on the tags used in the partup
-            var tags = partup.tags || [];
-            selector['profile.tags'] = {'$in': tags};
+            // Match the uppers on the provided tags
+            tags = tags || [];
+            selector['profile.tags'] = {$in: tags};
         }
 
         // Exclude the current logged in user from the results
-        selector['_id'] = {'$nin': [Meteor.userId()]};
+        selector['_id'] = {$nin: [Meteor.userId()]};
 
-        // Exclude existing partners from result, unless a search is happening
+        // Exclude provided uppers from result, unless a search is happening
         if (!searchOptionsProvided) {
-            selector['_id'] = {'$nin': partup.uppers};
+            excludedUppers = excludedUppers || [];
+            selector['_id'] = {$nin: excludedUppers};
         }
 
         // Sort the uppers on participation_score
-        options['sort'] = {'participation_score': -1};
+        options['sort'] = {participation_score: -1};
 
         // Only return the IDs
-        options['fields'] = {'_id': 1};
+        options['fields'] = {_id: 1};
 
         // Limit results
         options['limit'] = limit;
