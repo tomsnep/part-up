c43f97c8ba9080e4fe0fbdfd497e8b7d8435a562
Theuy Limpanont <theuy.limpanont@gmail.com>
2016-03-30 19:13:40 +0200

    feat(dropbox-chooser): add human readable filesize output for document renderer

--- app/packages/partup:lib/helpers/dropboxChooser.js
+++ app/packages/partup:lib/helpers/dropboxChooser.js
@@ -76,8 +76,18 @@ var DropboxRenderer = function () {
     return {
         getFileIdFromDirectLink: getFileIdFromDirectLink,
         createPreviewLinkFromDirectLink: createPreviewLinkFromDirectLink,
-        getSvgIcon: getSvgIcon
+        getSvgIcon: getSvgIcon,
+        bytesToSize: bytesToSize
     };
+
+    // from http://scratch99.com/web-development/javascript/convert-bytes-to-mb-kb/
+    function bytesToSize(bytes) {
+        var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+        if (bytes == 0) return 'n/a';
+        var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+        if (i == 0) return bytes + ' ' + sizes[i];
+        return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+    }
 
     function getFileIdFromDirectLink(fileUrl) {
         return fileUrl.match(/view\/(\w+)/)[1];
